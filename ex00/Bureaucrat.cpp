ae596bc0061b5c667ee6d5247a909850cc82f7e9
tndreka <tndreka@student.42heilbronn.de>
2025-07-28 13:18:57 +0200

    ADD: in the .Cpp  initialized the canonical form and in the header file also initialized that as well but added the clas to throw exceptions for the grade if it is low or if it is high

--- ex00/Bureaucrat.cpp
+++ ex00/Bureaucrat.cpp
@@ -3,10 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 14:41:06 by tndreka           #+#    #+#             */
-/*   Updated: 2025/07/26 16:45:40 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/07/28 13:16:54 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat()
+{
+	std::cout << "Default Bureaucrat constructor called" << std::endl;
+	_name = "";
+	grade = 150;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other)
+{
+	std::cout << "Bureaucrat Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	std::cout << "Bureaucrat Copy assignment operator called" << std::endl;
+	if (this != &other)
+	{
+		this->_name = other._name;
+		this->grade = other.grade;
+	}
+	return * this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "Bureaucrat Destructor called" << std::endl;
+}
--- ex00/Bureaucrat.hpp
+++ ex00/Bureaucrat.hpp
@@ -3,25 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tndreka <tndreka@student.42heilbronn.de    +#+  +:+       +#+        */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 14:41:12 by tndreka           #+#    #+#             */
-/*   Updated: 2025/07/26 20:57:02 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/07/27 20:45:50 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
 
+#include <iostream>
+#include <string>
+
 class Bureaucrat
-{
-    private:
-    
+{   
+	private:
+	std::string _name;
+	int grade;
+	// void setName(std::string str);
+	// void setGrade(int n);
     public:
     Bureaucrat();
+	Bureaucrat(std::string name, int grade);
     Bureaucrat(const Bureaucrat& other);
-    Bureaucrat operator=(const Bureaucrat& other);
+    Bureaucrat& operator=(const Bureaucrat& other);
     ~Bureaucrat();
-}
-
+	std::string getName();
+	int getGrade();
+	
+	class GradeTooHigh : public std::exception
+	{
+		public:
+		virtual const char* what() const noexcept;		
+	};
+	class GradeTooLow : public std::exception
+	{
+		public:
+		virtual const char* what() const noexcept;		
+	};
+};
 #endif
