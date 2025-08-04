e04f9335b6df1c3e7b50d2c9d1e394e2dd5a2dfd
tndreka <tndreka@student.42heilbronn.de>
2025-08-04 15:40:19 +0200

    ADD: Initialized the canonical form and in the end created a simple execute function without the tree that subject requires just too see is everything works.

--- ex02/ShrubberyCreationForm.cpp
+++ ex02/ShrubberyCreationForm.cpp
@@ -6,18 +6,26 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 17:20:55 by tndreka           #+#    #+#             */
-/*   Updated: 2025/08/02 18:01:44 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/08/04 15:34:05 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ShrubberyCreationForm.hpp"
 
-ShrubberyCreationForm::ShrubberyCreationForm()
+ShrubberyCreationForm::ShrubberyCreationForm() 
+: AForm("ShrubberyCreationForm", 145, 137), target("default")
 {
 	std::cout << "ShrubberyCreationForm Default constructor called\n";
 }
 
+ShrubberyCreationForm::ShrubberyCreationForm(const std::string& target_name) 
+: AForm("ShrubberyCreationForm", 145, 137), target(target_name)
+{
+	std::cout << "ShrubberyCreationForm Parameter constructor called\n";
+}
+
 ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other)
+: AForm(other), target(other.target)
 {
 	std::cout << "ShrubberyCreationForm Copy constructor called\n";
 }
@@ -25,9 +33,37 @@ ShrubberyCreationForm::ShrubberyCreationForm(const ShrubberyCreationForm& other)
 ShrubberyCreationForm& ShrubberyCreationForm::operator=(const ShrubberyCreationForm& other)
 {
 	std::cout << "ShrubberyCreationForm Copy assignment operator called\n";
+	if(this != &other)
+	{
+		AForm::operator=(other);
+		target = other.target;
+	}
+	return *this;
 }
 
 ShrubberyCreationForm::~ShrubberyCreationForm()
 {
 	std::cout << "ShrubberyCreationForm Destructor called\n";
+}
+
+void ShrubberyCreationForm::execute(Bureaucrat const & executor) const
+{
+	//First we call the base class to validate everything before the action executes
+	AForm::execute(executor);
+
+	//creating the file_name with the prefix
+	std::string file_name = target + "_shrubbery";
+	
+	//creating the file
+	std::ofstream file(file_name);
+	
+	if (file.is_open())
+	{
+		file << "TESTT\n";
+		file.close();
+	}
+	else
+	{
+		std::cout << "Error: could not create the file " << file_name << std::endl;	
+	}
 }
--- ex02/ShrubberyCreationForm.hpp
+++ ex02/ShrubberyCreationForm.hpp
@@ -6,7 +6,7 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/02 17:21:16 by tndreka           #+#    #+#             */
-/*   Updated: 2025/08/02 17:51:13 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/08/04 15:04:12 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 #define SHRUBBERYCREATIOMFORM_HPP
 
 #include "AForm.hpp"
-
+#include <fstream> // Liberary to create and write files
 class ShrubberyCreationForm : public AForm
 {
 private:
-	/* data */
+	std::string target;
 public:
 	ShrubberyCreationForm();
+	ShrubberyCreationForm(const std::string& target_name);
 	ShrubberyCreationForm(const ShrubberyCreationForm& other);
 	ShrubberyCreationForm& operator=(const ShrubberyCreationForm& other);
 	~ShrubberyCreationForm();
+	void execute(Bureaucrat const & executor) const override;
 };
 
 #endif
