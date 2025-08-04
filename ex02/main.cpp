86bfa4857c7dafc8d2a140ba325251e4fd080264
tndreka <tndreka@student.42heilbronn.de>
2025-08-04 15:41:11 +0200

    created one succesful test case for the shrubbery form to check if the scenario works and behaves how it should

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,13 +6,13 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/26 14:39:19 by tndreka           #+#    #+#             */
-/*   Updated: 2025/08/03 18:22:20 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/08/04 15:23:28 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "AForm.hpp"
-
+#include "ShrubberyCreationForm.hpp"
 
 int main()
 {
@@ -96,109 +96,127 @@ int main()
 		std::cerr << e.what() << '\n';
 	}
 
-	std::cout << "================ FORM TEST =================== \n";
+	// std::cout << "================ FORM TEST =================== \n";
 	
-	std::cout << " ------- TEST 1 -------------\n";
-	try
-	{
-		Bureaucrat john("John", 50);
-		// Form tax("Tax", 40, 30);
+	// std::cout << " ------- TEST 1 -------------\n";
+	// try
+	// {
+	// 	Bureaucrat john("John", 50);
+	// 	// Form tax("Tax", 40, 30);
 		
-		std::cout << john << std::endl;
-		// std::cout << tax  << std::endl;
+	// 	std::cout << john << std::endl;
+	// 	// std::cout << tax  << std::endl;
 
-		// john.signForm(tax);
-	// /	std::cout << tax  << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+	// 	// john.signForm(tax);
+	// // /	std::cout << tax  << std::endl;
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
 	
-	std::cout << " ------- TEST 2 -------------\n";
-	try
-	{
-		Bureaucrat bond("John", 80);
-		// Form taxb("Tax", 1, 151);
+	// std::cout << " ------- TEST 2 -------------\n";
+	// try
+	// {
+	// 	Bureaucrat bond("John", 80);
+	// 	// Form taxb("Tax", 1, 151);
 		
-		std::cout << bond << std::endl;
-		// std::cout << taxb  << std::endl;
+	// 	std::cout << bond << std::endl;
+	// 	// std::cout << taxb  << std::endl;
 
-		// bond.signForm(taxb);
-		// std::cout << taxb  << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+	// 	// bond.signForm(taxb);
+	// 	// std::cout << taxb  << std::endl;
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
 	
-	std::cout << " ------- TEST 3 -------------\n";
-	try
-	{
-		Bureaucrat james("John", 50);
-		// AForm tax_form("Tax", 0, 30);
+	// std::cout << " ------- TEST 3 -------------\n";
+	// try
+	// {
+	// 	Bureaucrat james("John", 50);
+	// 	// AForm tax_form("Tax", 0, 30);
 		
-		std::cout << james << std::endl;
-		// std::cout << tax_form  << std::endl;
+	// 	std::cout << james << std::endl;
+	// 	// std::cout << tax_form  << std::endl;
 
-		// james.signForm(tax_form);
-		// std::cout << tax_form  << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+	// 	// james.signForm(tax_form);
+	// 	// std::cout << tax_form  << std::endl;
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
 	
-	std::cout << " ------- TEST 4 -------------\n";
-	try
-	{
-		Bureaucrat jim("John", 50);
-		// Form bean("Tax", 40, 200);
+	// std::cout << " ------- TEST 4 -------------\n";
+	// try
+	// {
+	// 	Bureaucrat jim("John", 50);
+	// 	// Form bean("Tax", 40, 200);
 		
-		std::cout << jim << std::endl;
-		// std::cout << bean  << std::endl;
+	// 	std::cout << jim << std::endl;
+	// 	// std::cout << bean  << std::endl;
 
-		// jim.signForm(bean);
-		// std::cout << bean  << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+	// 	// jim.signForm(bean);
+	// 	// std::cout << bean  << std::endl;
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
 	
-	std::cout << " ------- TEST 5 -------------\n";
-	try
-	{
-		Bureaucrat jack("John", 50);
-		// Form daniels("Tax", 40, 30);
-		// 
-		std::cout << jack << std::endl;
-			// std::cout << daniels  << std::endl;
+	// std::cout << " ------- TEST 5 -------------\n";
+	// try
+	// {
+	// 	Bureaucrat jack("John", 50);
+	// 	// Form daniels("Tax", 40, 30);
+	// 	// 
+	// 	std::cout << jack << std::endl;
+	// 		// std::cout << daniels  << std::endl;
 
-		// jack.signForm(daniels);
-		// std::cout << daniels  << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
-	std::cout << " ------- TEST 6 -------------\n";
-	try
-	{
-		Bureaucrat ceo("CEO", 1);
-		// Form		boss("BOSS", 50, 50);
+	// 	// jack.signForm(daniels);
+	// 	// std::cout << daniels  << std::endl;
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
+	// std::cout << " ------- TEST 6 -------------\n";
+	// try
+	// {
+	// 	Bureaucrat ceo("CEO", 1);
+	// 	// Form		boss("BOSS", 50, 50);
 		
-		std::cout << ceo << std::endl;
-		// std::cout << boss << std::endl;
+	// 	std::cout << ceo << std::endl;
+	// 	// std::cout << boss << std::endl;
 
-		// ceo.signForm(boss);
-		// std::cout << boss << std::endl;
+	// 	// ceo.signForm(boss);
+	// 	// std::cout << boss << std::endl;
+	// }
+	// catch(const std::exception& e)
+	// {
+	// 	std::cerr << e.what() << '\n';
+	// }
+	
+	std::cout << " ------- TEST SHRUBBERY -------------\n";
+	try
+	{
+		Bureaucrat scenario_one("One", 100);
+		ShrubberyCreationForm shrub("bureaucrat");
+		
+		std::cout << scenario_one << std::endl;
+		std::cout << shrub << std::endl;
+
+		scenario_one.signForm(shrub);
+		std::cout << shrub << std::endl;
+		
+		scenario_one.executeForm(shrub);
 	}
 	catch(const std::exception& e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	
 	
     return 0;
 }
