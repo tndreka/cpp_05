9764f886bd1e004e1e69125e3bea6e78ea362764
tndreka <tndreka@student.42heilbronn.de>
2025-08-05 15:58:59 +0200

    ADD: initializing the canonical form in Intern class and to implement the makeForm()

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,37 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 15:35:27 by tndreka           #+#    #+#             */
-/*   Updated: 2025/08/05 15:35:28 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/08/05 15:58:14 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Intern.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
+
+
+Intern::Intern()
+{
+	std::cout<< "Intern Default constructor called" <<std::endl;	
+}
+
+Intern::Intern(const Intern& other)
+{
+	std::cout<< "Intern Copy Constructor called" <<std::endl;
+}
+
+Intern& Intern::operator=(const Intern& other)
+{
+	std::cout<< "Intern Copy assignment operator called" <<std::endl;	
+}
+
+Intern::~Intern()
+{
+	std::cout<< "Intern Destructor called" <<std::endl;	
+}
+
+AForm* Intern::makeForm(std::string form_name, std::string form_target) const
+{
+	
+}
